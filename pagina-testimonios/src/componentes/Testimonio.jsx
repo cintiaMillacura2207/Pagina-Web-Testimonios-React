208038a73ef3e844f197ce0955f72ea4b65ce3be
cintiaMillacura22207 <37818974+cintiaMillacura2207@users.noreply.github.com>
2024-08-13 18:59:23 -0300

    Crear estructura del Componente

--- pagina-testimonios/src/componentes/Testimonio.jsx
+++ pagina-testimonios/src/componentes/Testimonio.jsx
@@ -1,2 +1,24 @@
 // se importa react del paquete react
 import React from "react";
+import '../hojas-de-estilo/testimonio.css';
+
+function Testimonio(props){
+    // estructura del componente
+    return (
+        <div className="contenedor-testimonio">
+            <img 
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt={`Fotografia de ${props.imagen}`} />
+      <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>
+            <strong>{props.nombre}</strong> en {props.pais}
+          </p>
+          <p className='cargo-testimonio'>
+            {props.cargo} en <strong>{props.empresa}</strong>
+          </p>
+          <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
+        </div>
+    );
+}
